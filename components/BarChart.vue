f0bc1fd70b265ec3706c8916c34f8adca6172923
Darkhorse <horsedark374@gmail.com>
2025-06-12 18:06:36 -0500

    BarChart

---
+++ components/BarChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <Bar :data="data" :options="options" />
+</template>
+
+<script setup>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+defineProps({
+  data: {
+    type: Object,
+    required: true
+  },
+  options: {
+    type: Object,
+    required: true
+  }
+})
+</script>
