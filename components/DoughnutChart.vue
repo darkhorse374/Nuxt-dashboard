836e3ecff0d8dbc5233209b4055f8b1b6be30215
Darkhorse <horsedark374@gmail.com>
2025-06-12 18:08:31 -0500

    DoughnutChart

---
+++ components/DoughnutChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <Doughnut :data="data" :options="options" />
+</template>
+
+<script setup>
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+defineProps({
+  data: {
+    type: Object,
+    required: true
+  },
+  options: {
+    type: Object,
+    required: true
+  }
+})
+</script>
